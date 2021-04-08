3ea2ba53a365fa689922a094563f33b639ec1cf2
Winfred Huang <huang.winfred21@gmail.com>
2021-04-08 14:54:56 -0700

    Set up question routes and model

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const questions = require('./routes/api/questions');
+
+app.use(express.json())
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connection established!"))
     .catch(err => console.log(err));
 
+
 app.get('/', (req, res) => res.send("This works!"));
+app.use('/api/questions', questions);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
